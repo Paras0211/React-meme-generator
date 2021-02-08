9031471472cd019b43e411cf4e826be985020b22
Paras0211 <72230187+Paras0211@users.noreply.github.com>
2021-02-08 17:19:35 +0530

    Update MemeGenerator.js

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -6,7 +6,7 @@ class MemeGenerator extends Component{
         this.state ={
             topText : "",
             bottomText : "",
-            randomImg : "http://i.imgflip.com/1bij.jpg",
+            randomImg : "",
             allMemeImg : []
         }
         
