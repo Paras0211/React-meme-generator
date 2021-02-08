fe3981f098bf8992cb0c1a7861c1065747670674
Paras0211 <72230187+Paras0211@users.noreply.github.com>
2021-02-08 15:37:16 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" height="100px" width="100px" alt="No Pic" />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
